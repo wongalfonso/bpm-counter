c050cf036bffb010188ad1a7d212b2255fac4c82
wongalofonso <alfonsowongsd@gmail.com>
2019-03-13 10:17:44 -0700

    added server port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 if (process.env !== 'develpment') {
   app.use(morgan());
@@ -9,4 +10,7 @@ if (process.env !== 'develpment') {
   app.use(morgan('dev'));
 }
 app.use(express.static('public'));
+app.use(express.static('dist'));
+
+app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 
